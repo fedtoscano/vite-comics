bed39e3f5d7f939c3e4ffd5356d92887d93cdf27
fedtoscano <federicotoscano611@gmail.com>
2024-06-11 14:20:47 +0200

    fixato bordo inferiore del jumbotron

--- vite-comics/src/components/AppJumbotron.vue
+++ vite-comics/src/components/AppJumbotron.vue
@@ -7,13 +7,20 @@ return {
 </script>
 
 <template>
-<img src="../assets/img/jumbotron.jpg" alt="">
+    <div class="img-container">
+        <img src="../assets/img/jumbotron.jpg" alt="">
+    </div>
 </template>
 
 <style scoped>
-
-    img{
+    .img-container{
         width: 100%;
         height: 500px;
+
+    }
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
 </style>
